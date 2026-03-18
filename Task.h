1f1233ffaa2d0f260fe4e64b021a9cadfc70ddcf
Lameea Mubashir Khan <lameeamubashirkhan@gmail.com>
2026-03-18 15:32:28 +0500

    Add files via upload

---
+++ Task.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <string>
+#include <vector>
+using namespace std;
+struct Task
+{
+	string description;
+	bool done;
+};
+
+void SaveTasksToFile(const vector<Task>& tasks, const string& fileName);
+vector<Task> loadTasksFromFile(const string& fileName);
